d67e08088fe83203bd80299632b3423c4e26e137
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-09-16 23:07:20 +0530

    Module concept

---
+++ module/reusable.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def main():
+    print("Testing my functions at top level", square(5), cube(9))
+
+def square(p):
+    return p ** 2
+
+def cube(p):
+    return p ** 3
+
+if __name__ == "__main__":
+    main()
