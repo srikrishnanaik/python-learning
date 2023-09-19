235eefd2c78c7542912d06cfb2380e22c3bfc777
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-09-20 00:34:58 +0530

    Class @property decorator

---
+++ class/student3_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from student3 import Student
+
+def main():
+    jeff = Student("Jeff", "Americal History")
+    heather = Student("Heather", "Mathematics")
+    print(jeff.name, ":", jeff.major)
+    jeff.name = "Jeffrey"
+    print(jeff.name, ":", jeff.major)
+    heather.major = "Computer Science"
+    print(heather.name, ":", heather.major)
+
+if __name__ == "__main__":
+    main()
