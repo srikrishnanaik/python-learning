34a8e473580e615e3b0bf6804f856a4969e10c7b
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-09-19 21:17:56 +0530

    Student1 Class

---
+++ class/student1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+class Student:
+    def __init__(self, name, major):
+        self.name = name
+        self.major = major
+    
+def main():
+    jeff = Student("Jeff", "Americal History")
+    heather = Student("Heather", "Mathematics")
+    print(jeff.name, ":", jeff.major)
+    print(heather.name, ":", heather.major)
+    jeff.name = "Jeffrey"
+    heather.major = "Computer science"
+    print(jeff.name, ":", jeff.major)
+    print(heather.name, ":", heather.major)
+
+if __name__ == "__main__":
+    main()
