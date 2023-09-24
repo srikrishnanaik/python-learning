8722ab8f588e1d1f7ed0ba2eb7f3d45575377dcd
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-09-24 21:20:45 +0530

    Exceptions

---
+++ Exceptions/multi.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+def main():
+    names = ['Mike','John','Jane','Alice']
+    themap = {'Mike': 15, 'Chris': 10, 'Dave': 25}
+    
+    while True:
+        try:
+            value = input("Please enter an integer:")
+            if value == "end":
+                break
+            value = int(value)
+            print("Name is: " + names[value])
+            name = input("Enter a name: ")
+            print(name, " => ", themap(name))
+        except ValueError:
+            print("Value Error: non numeric data")
+        except (KeyError, IndexError) as err:
+            print("Illegal value: ", err)
+        except Exception:
+            print("Unknown Exception: ")
+
+if __name__ == "__main__":
+    main()
+
