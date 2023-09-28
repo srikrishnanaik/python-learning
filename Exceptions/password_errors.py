00c30ff03755b341e7bb408a3dd06250712126aa
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-09-28 13:56:07 +0530

    Exceptions in Python

---
+++ Exceptions/password_errors.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+class PasswordError(Exception):
+    """Base class for exceptions in this module"""
+    pass
+
+class TrivialPasswordError(PasswordError):
+    """Passwords that are too Trivial like: 'password'"""
+    def __init__(self, msg):
+        super().__init__("Trivial password: "+msg)
+
+class PasswordLengthError(PasswordError):
+    """Password that do not meet certain length criteria"""
+    def __init__(self, msg, length):
+        super().__init__(msg)
+        self.length = length
+
+    def get_length(self):
+        return self.length
+    
+class RepetetiveError(PasswordError):
+    """Passwords that have repetetive characters"""
+    def __init__(self, msg):
+        super().__init__(msg)
---
+++ Exceptions/password_tests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from password_errors import PasswordError
+from password_utility import check_trivial, check_length, check_duplicates
+
+def check_password(password):
+    check_trivial(password)
+    check_length(password)
+    check_duplicates(password)
+
+def main():
+    while True:
+        try:
+            line = input("Please enter a password: ")
+            check_password(line)
+            print("That would be a valid password")
+        except PasswordError as pe:
+            print("Password Error: ", pe)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        print("Terminating program")
+    except Exception as e:  
+        print("Unknown issue: ", e)
