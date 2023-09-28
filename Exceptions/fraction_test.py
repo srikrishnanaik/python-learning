a4076ca923c27d543323955963ed046c8cef0c5c
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-09-28 10:59:05 +0530

    Exceptions in Python

---
+++ Exceptions/fraction_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from fraction import Fraction
+
+def main():
+    try:
+        while True:
+            numer = int(input("Please enter a numerator"))
+            denom = int(input("Please enter a denominator"))
+            fraction = Fraction(numer, denom)
+            print(fraction)
+    except ZeroDivisionError:
+        print("Zero Devision Error")
+
+if __name__ == "__main__":
+    main()
